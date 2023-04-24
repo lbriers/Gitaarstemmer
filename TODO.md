5095c639c5b98cd4f461845a303e69e5e1df8924
JordyMeier <124741325+JordyMeier@users.noreply.github.com>
2023-04-24 23:34:03 +0200

    toevoegen view binding

--- TODO.md
+++ TODO.md
@@ -1,4 +1,17 @@
-TODO:	
-lessen nakijken;	
-uitzoeken analyseren van geluid
+# TODO:
 
+- lessen nakijken;
+- uitzoeken analyseren van geluid
+
+# Minimum requirements
+
+- At least three fragments or activities;
+- At least one data storage/request system;
+- At least one master/detail using a RecyclerView;
+- At least one Intent;
+- At least one menu option;
+- At least one data signal to be used (GPS, Camera, accelerometer, …);
+- At least one extra feature you can freely choose. For instance:
+    - Authentication through Google’s Play Store or Firebase
+    - Sharing data between apps using a ContentProvider
+    - Etc… See links for more inspiration on what’s possible.
--- app/src/main/java/com/example/gitaarstemmer/MainActivity.kt
+++ app/src/main/java/com/example/gitaarstemmer/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.gitaarstemmer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gitaarstemmer.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //binding test
+        binding.button.setOnClickListener { view ->
+            Snackbar.make(view, "Yippie!", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+
     }
 }
