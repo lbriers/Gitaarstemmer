351d3c179ce899d7e4f85ce0acb7d6bc51cd5705
JordyMeier <124741325+JordyMeier@users.noreply.github.com>
2023-06-01 15:38:05 +0200

    feedback juiste noot gespeeld

--- TODO.md
+++ TODO.md
@@ -1,8 +1,3 @@
-# TODO:
-
-- lessen nakijken;
-- uitzoeken analyseren van geluid
-
 # Minimum requirements
 
 - [X] At least three fragments or activities;
@@ -10,8 +5,8 @@
 - [X] At least one master/detail using a RecyclerView;
 - [X] At least one Intent;
 - [x] At least one menu option;
-- [ ] At least one data signal to be used (GPS, Camera, accelerometer, …);
-- [ ] At least one extra feature you can freely choose. For instance:
+- [X] At least one data signal to be used (GPS, Camera, accelerometer, …);
+- [X] At least one extra feature you can freely choose. For instance:
     - Authentication through Google’s Play Store or Firebase
     - Sharing data between apps using a ContentProvider
     - Etc… See links for more inspiration on what’s possible.
--- app/src/main/java/com/example/gitaarstemmer/StemmerFragment.kt
+++ app/src/main/java/com/example/gitaarstemmer/StemmerFragment.kt
@@ -1,59 +1,102 @@
 package com.example.gitaarstemmer
 
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Html
+import android.text.Spanned
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.gitaarstemmer.databinding.FragmentStemmerBinding
 
 
 class StemmerFragment(var stemmingenLijst: ArrayList<Stemming>) : Fragment() {
 
     private lateinit var binding: FragmentStemmerBinding
-    private val mainHandler = Handler(Looper.getMainLooper())
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        stemmerThread.start()
-        super.onCreate(savedInstanceState)
-    }
+    // thread
+    private val mainHandler = Handler(Looper.getMainLooper())
+    private var active: Boolean = false // is dit fragment bezig
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentStemmerBinding.inflate(layoutInflater)
+        active = true
 
-        var stemming: Stemming? = stemmingenLijst.firstOrNull{ it.selected } // kan null zijn
-        if(stemming == null){
-            binding.snarenNotatie.text = getString(R.string.no_stemming_selected_message)
+        var stemming: Stemming? = stemmingenLijst.firstOrNull { it.selected } // kan null zijn
+        if (stemming == null) {
+            binding.stemmerTitel.text = getString(R.string.no_stemming_selected_message)
+            binding.snarenNotatie.text = ""
+        } else {
+            binding.stemmerTitel.text = stemming.title
+            binding.snarenNotatie.text = snarenNotatieText(stemming, 0)
         }
-        else{
-            var noten = stemming.noten
-            binding.snarenNotatie.text = noten.get(0).toString() + " - " + noten.get(1).toString() +  " - " + noten.get(2).toString() +  " - " + noten.get(3).toString() +  " - " + noten.get(4).toString() +  " - " + noten.get(5).toString()
 
-        }
+        createThread(stemming).start()
 
         return binding.root
     }
 
-    val stemmerThread = Thread {
-        val noteFinder = NoteFinder()
-        val noten = arrayOf("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#")
-        while (true) {
-            var frequency = noteFinder.getNote()
-            mainHandler.post {
-                binding.noteTeller.text = noten[frequency[0]]
-                binding.noteOffset.text = frequency[1].toString()
+    override fun onDestroyView() {
+        active = false // stopt de thread
+        super.onDestroyView()
+    }
+
+    private fun snarenNotatieText(stemming: Stemming, hoeveelJuisteNoten: Int): Spanned {
+        var returnText: String = ""
+        var green = "#32CD32"
+        var standard = "@android:color/tab_indicator_text"
+        for (i in 0..5) {
+            // bron: https://stackoverflow.com/a/30859757
+            if (i < hoeveelJuisteNoten) {
+                returnText += "<font color=" + green + ">" + stemming.noten[i].toString() + "</font> "
+            } else {
+                returnText += "<font color=" + standard + ">" + stemming.noten[i].toString() + "</font> "
             }
-            Thread.sleep(750)
+
+        }
+        return Html.fromHtml(returnText)
+    }
+
+    private fun createThread(stemming: Stemming?): Thread {
+        return Thread {
+            val noteFinder = NoteFinder()
+            val noten = arrayOf("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#")
+
+            // voor de feedback
+            var tel = 0 // telt hoevaak de juiste noot is gespeeld
+            var hoeveelsteNoot = 0 // de noot die nu gecheckt wordt
+
+            // thread loop
+            while (active) {
+                var frequency = noteFinder.getNote()
+                if (stemming != null && hoeveelsteNoot < 6) {
+                    if (noten[frequency[0]] == stemming.noten[hoeveelsteNoot].toString() && frequency[1] == 0) {
+                        tel++
+                        if (tel == 3) {
+                            tel = 0
+                            hoeveelsteNoot++
+                        }
+                    } else {
+                        tel = 0
+                    }
+                }
+
+                mainHandler.post {
+                    if (stemming != null) {
+                        binding.snarenNotatie.text = snarenNotatieText(stemming, hoeveelsteNoot);
+                    }
+                    binding.noteTeller.text = noten[frequency[0]]
+                    binding.noteOffset.text = frequency[1].toString()
+                }
+                Thread.sleep(750)
+            }
         }
     }
+
 
 }
